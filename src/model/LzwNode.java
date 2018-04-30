1954b445480b3eb6e4fc96cfd29a0e4ef314f5a0
Janyelson Victor Lacerda de Oliveira <janyelsonvictor@gmail.com>
2018-04-29 21:44:59 -0300

    Mudança: Algoritmo de busca de um nó por simbolo e busca do index por simbolo são apenas por largura (apenas entre os filhos do nó atual)

--- src/model/LzwNode.java
+++ src/model/LzwNode.java
@@ -27,38 +27,12 @@ public class LzwNode{
      * @return
      */
     public int findSymbolIndex(int searchSymbol) {
-        if(this.symbol == searchSymbol)
-            return index;
-        else{
-
-            for (LzwNode node : children){
-                int symbolIndex = node.findSymbolIndex(searchSymbol);
-                if(symbolIndex != -1)
-                    return symbolIndex;
-            }
-
-            return -1;
+        for (LzwNode node : children){
+            if(node.getSymbol() == searchSymbol)
+                return node.getIndex();
         }
-    }
 
-    /**
-     * Realiza a busca do nó pelo índice e retorna-o, caso exista, caso contrário, retorna null.
-     *
-     * @param searchIndex
-     * @return
-     */
-    public LzwNode getNodeByIndex(int searchIndex){
-        if(this.index == searchIndex)
-            return this;
-        else {
-            for (LzwNode node : children){
-                LzwNode indexedChild = node.getNodeByIndex(searchIndex);
-                if(indexedChild != null)
-                    return indexedChild;
-            }
-
-            return null;
-        }
+        return -1;
     }
 
     /**
@@ -68,17 +42,13 @@ public class LzwNode{
      * @return
      */
     public LzwNode getNodeBySymbol(int searchSymbol){
-        if(this.symbol == searchSymbol)
-            return this;
-        else {
-            for (LzwNode node : children){
-                LzwNode symbolNode = node.getNodeBySymbol(searchSymbol);
-                if(symbolNode != null)
-                    return symbolNode;
-            }
-
-            return null;
+        for (LzwNode node : children){
+            if(node.getSymbol() == searchSymbol)
+                return node;
         }
+
+        return null;
+
     }
 
     public void addChild(int index, int symbol) {
